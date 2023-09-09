bda3428819d714d665292b180253b935286eec61
zadr0tishe <112801448+zadr0tishe@users.noreply.github.com>
2023-09-09 14:41:54 +0300

    implemented core job listing page logic, started working on search filter

--- client/src/app/job-listings/page.tsx
+++ client/src/app/job-listings/page.tsx
@@ -2,6 +2,7 @@
 import jobs from '@/api/jobs.json';
 import JobListing from '@/components/JobListing';
 import Pagination from '@/components/Pagination';
+import SearchBar from '@/components/SearchBar';
 import useJobListings from '@/hooks/useJobListings';
 import { useState } from 'react';
 
@@ -9,6 +10,9 @@ export default function Page() {
   const [page, setPage] = useState(1);
   const resultsPerPage = 10;
   const { data, isLoading, isError, error } = useJobListings();
+
+  const [searchTerm, setSearchTerm] = useState('');
+  const [newData, setNewData] = useState(data);
 
   if (isLoading) {
     return <p>Loading...</p>;
@@ -19,32 +23,44 @@ export default function Page() {
   }
 
   return (
-    <div className='flex flex-col gap-6 border-white border-solid border-2 rounded-md shadow-lg'>
-      {data?.map((job: JobListing, idx: number) => {
-        if (
-          idx < page * resultsPerPage &&
-          idx > (page - 1) * resultsPerPage - 1
-        ) {
-          return (
-            <JobListing
-              id={job.id}
-              key={job.id}
-              jobTitle={job.jobTitle}
-              company={job.company}
-              location={job.location}
-              salaryRange={job.salaryRange}
-              applicationDeadline={job.applicationDeadline}
-              jobType={job.jobType}
-            />
-          );
-        }
-      })}
-      <Pagination
-        jobListings={jobs}
-        resultsPerPage={resultsPerPage}
-        page={page}
-        setPage={setPage}
-      />
-    </div>
+    <section className=''>
+      <div className='m-auto max-w-7xl py-4 px-8'>
+        <SearchBar setSearchTerm={setSearchTerm} />
+        <div className='flex flex-col rounded-md shadow-lg overflow-hidden my-2 '>
+          {data?.map((job: JobListing, idx: number) => {
+            if (
+              idx < page * resultsPerPage &&
+              idx > (page - 1) * resultsPerPage - 1
+            ) {
+              if (
+                job.jobTitle.toLowerCase().includes(searchTerm.toLowerCase()) ||
+                job.company.toLowerCase().includes(searchTerm.toLowerCase()) ||
+                job.jobType.toLowerCase().includes(searchTerm.toLowerCase()) ||
+                job.location.toLowerCase().includes(searchTerm.toLowerCase())
+              ) {
+                return (
+                  <JobListing
+                    id={job.id}
+                    key={job.id}
+                    jobTitle={job.jobTitle}
+                    company={job.company}
+                    location={job.location}
+                    salaryRange={job.salaryRange}
+                    applicationDeadline={job.applicationDeadline}
+                    jobType={job.jobType}
+                  />
+                );
+              }
+            }
+          })}
+          <Pagination
+            jobListings={jobs}
+            resultsPerPage={resultsPerPage}
+            page={page}
+            setPage={setPage}
+          />
+        </div>
+      </div>
+    </section>
   );
 }
